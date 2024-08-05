ba8d3728c8ab8517d598c9b777c099ee07abb6df
khash <khashayar1924@gmail.com>
2024-08-05 14:15:26 -0400

    update graph scripts

--- single _run/model.bat
+++ single _run/model.bat
@@ -1,1 +1,1 @@
-python .\..\pest_eeek.py --input=eeek_input.csv --output=eeek_output.csv --points=points.csv --num_sinusoid_pairs=1 --include_intercept --store_measurement --collection=L8_L9_GC
+python .\..\pest_eeek.py --input=eeek_input.csv --output=eeek_output.csv --points=points.csv --num_sinusoid_pairs=1 --include_intercept --store_measurement --collection=L8_L9_GC --store_date
