123541f0b2c0cea02a032b9b141e927b72d220e8
Khash <khashayar1924@gmail.com>
2024-12-27 12:39:40 -0500

    cleanup test runs

--- pest/pest runs/set 5 - 5 points/IA - initial params v1/model.bat
+++
@@ -1,1 +0,0 @@
-python C:\Users\kazad\OneDrive\Documents\GitHub\eeek\pest_eeek.py --input=pest_input.csv --output=pest_output.csv --points=points.csv --num_sinusoid_pairs=1 --include_intercept --store_measurement --collection=L8_L9_2022_2023 --store_estimate --store_date --store_amplitude
